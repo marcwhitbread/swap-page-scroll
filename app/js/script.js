1f2cefc43bb459846601fb50aa64d94af201a9e5
Marc Whitbread <marcwhitbread@gmail.com>
2015-01-31 14:16:01 -0500

    Added page swap + drawer

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,36 @@
 $(document).ready(function() {
 
-	$('#switch .item').each(function(i) {
-		
-		$(this).on('click', { num: i }, function(e) {
-			
-			$('.layer').removeClass('current').eq(e.data.num).addClass('current');
-			
-		});
-		
-	});
+	
 	
 });
+
+function close_all() {
+	
+	$('body').removeClass('menu-open').removeClass('overlay-1');
+	
+	$('.content-layer').not('.overlay-1').css('max-height', 'none');
+	$('.layer').not('.menu').css('max-height', 'none');
+	
+	window.scrollTo(0, 0);
+	
+}
+
+function menu_op() {
+	close_all();
+	
+	$('body').addClass('menu-open');
+	
+	$('.layer').not('.menu').css('max-height', $('.layer.menu').height() + 'px');
+	
+	window.scrollTo(0, 0);
+}
+
+function overlay_op() {
+	close_all();
+	
+	$('body').addClass('overlay-1');
+	
+	$('.content-layer').not('.overlay-1').css('max-height', $('.content-layer.overlay-1').height() + 'px');
+	
+	window.scrollTo(0, 0);
+}
