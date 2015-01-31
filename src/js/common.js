78e78622868bab0ce65f9857f943cae397cca72e
Marc Whitbread <marcwhitbread@gmail.com>
2015-01-31 11:34:27 -0500

    added working example

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-	
-	
+
+	$('#switch .item').each(function(i) {
+		
+		$(this).on('click', { num: i }, function(e) {
+			
+			$('.layer').removeClass('current').eq(e.data.num).addClass('current');
+			
+		});
+		
+	});
 	
 });
